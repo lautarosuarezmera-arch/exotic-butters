634059d3bd611040690ef1c9d4fdacf52e27b340
Alumno <alumno@et26.local>
2026-03-30 17:40:58 -0300

    Introduccion

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function MostrarMeses() {
+const Meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    console.log(Meses)
+}
+
